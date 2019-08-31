06736d1301efda231ee0f6d9e318e191d4bfef52
Roberto Barbosa <robertojf95@gmail.com>
2019-08-31 11:15:40 -0300

    adding new stack

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -4,9 +4,10 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
-import NewsScreen from '../screens/NewsScreen';
+import NewsScreen from '../screens/News/NewsScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import NewsDetailScreen from '../screens/News/NewsDetailScreen';
 
 const config = Platform.select({
   web: { headerMode: 'screen' },
@@ -16,6 +17,7 @@ const config = Platform.select({
 const NewsStack = createStackNavigator(
   {
     News: NewsScreen,
+    Detail: NewsDetailScreen
   },
   config
 );
