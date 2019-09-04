821dbf3518d73d036fc5ecc4bb8eaf620cfba289
Roberto Barbosa <robertojf95@gmail.com>
2019-09-04 19:43:29 -0300

    create new stacks

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import MainTabNavigator from './MainTabNavigator';
+import SignInScreen from '../screens/SignInScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 
+
+const AuthStack = createStackNavigator({
+  SignIn: SignInScreen,
+  SignUp: SignUpScreen
+});
+const AuthLoadingStack = createStackNavigator({
+  AuthLoading: AuthLoadingScreen
+})
 export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Main: MainTabNavigator,
-  })
+    AuthLoading: AuthLoadingStack,
+    App: MainTabNavigator,
+    Auth: AuthStack
+  }, {
+      initialRouteName: 'AuthLoading'
+    })
 );
