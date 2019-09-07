3099623d70d2165a2f125ef4b245722476c8d151
Roberto Barbosa <robertojf95@gmail.com>
2019-09-07 13:34:46 -0300

    adding about text

--- screens/Settings/AboutScreen.js
+++ screens/Settings/AboutScreen.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { List, ListItem, Text, Left, Right, Icon } from 'native-base';
+import { Text } from 'native-base';
 
 
 const AboutScreen = () => {
   return (<View style={styles.container}>
-    <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
+    <Text>O AvisaAi é uma aplicativo móvel que tem como principal objetivo Informar e Alertar a população sob possíveis risco, tais como: alagamento, fortes chuvas, deslizamento de terra e zonas de risco. </Text>
   </View>);
 }
 
